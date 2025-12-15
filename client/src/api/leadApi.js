9a66421c11e289377bcb281778311fce6f28955b
Sunil kannaujiya <skannaujiya920@gmail.com>
2025-12-16 01:38:02 +0530

    Refactor BASE_URL to use environment variable
    Removed hardcoded BASE_URL and used environment variable instead.

--- client/src/api/leadApi.js
+++ client/src/api/leadApi.js
@@ -1,12 +1,8 @@
 import axios from "axios";
 
-<<<<<<< HEAD
 const BASE_URL = import.meta.env.VITE_BASE_URL;
-=======
-const BASE_URL = "https://lead-management-system-gya5.onrender.com";
->>>>>>> 0f2eb9a7752a19fb1446fe1867e5c8e291222e9c
 
-// Get all leads
+
 export const getLeads = async () => {
   try {
     const res = await axios.get(`${BASE_URL}/api/leads`);
@@ -17,7 +13,7 @@ export const getLeads = async () => {
   }
 };
 
-// Add new lead
+
 export const addLead = async (lead) => {
   try {
     const res = await axios.post(`${BASE_URL}/api/leads`, lead);
