38dcd4958d32fe89bb9c4e8444e6886129f669cf
Sunil kannaujiya <skannaujiya920@gmail.com>
2025-12-16 01:36:27 +0530

    Update metaApi.js

--- client/src/api/metaApi.js
+++ client/src/api/metaApi.js
@@ -1,12 +1,8 @@
 import axios from "axios";
 
-<<<<<<< HEAD
 const BASE_URL = import.meta.env.VITE_BASE_URL;
-=======
-const BASE_URL = "https://lead-management-system-gya5.onrender.com";
->>>>>>> 0f2eb9a7752a19fb1446fe1867e5c8e291222e9c
 
-// Fetch a new dummy Meta lead
+
 export const fetchMetaLead = async () => {
   try {
     const res = await axios.get(`${BASE_URL}/api/meta/fetch`);
